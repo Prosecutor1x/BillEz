33b91a0e3fa4ca9a505cbb902a1d5a7b7fbc0288
Prosecutor1x <krishnarjunnanda@gmail.com>
2023-02-24 20:38:07 +0530

    router added

--- App.tsx
+++ App.tsx
@@ -5,31 +5,29 @@
  * @format
  */
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-
-import { Dimensions, useColorScheme, View} from 'react-native';
+import {Dimensions, useColorScheme, View} from 'react-native';
 
 import Home from './src/screens/home.main';
-import { StatusBar,Text } from 'react-native';
-import { horizontalScale, moderateScale, verticalScale }  from './theme/theme.metrics'
-import { NavigationContainer } from '@react-navigation/native';
+import {StatusBar, Text} from 'react-native';
+import {
+  horizontalScale,
+  moderateScale,
+  verticalScale,
+} from './theme/theme.metrics';
+import {NavigationContainer} from '@react-navigation/native';
 import Navigation from './src/navigation/navigation';
-
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
- 
-
 
   return (
-    <View className={`flex-1 justify-center items-center  h-${verticalScale(70) } w-${horizontalScale(150)} text-${moderateScale(24)} `}>
-      <NavigationContainer>
-        {/* <StatusBar />
-        <Home /> */}
-        <Navigation/>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      
+        <Navigation />
+   
+    </NavigationContainer>
   );
 }
 export default App;
--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -1,19 +1,17 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../screens/home.main';
 import Onboarding from '../screens/onboarding.main';
-
+import {NavigationContainer} from '@react-navigation/native';
 
 const Navigation = () => {
-    const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
   return (
-    <View>
-       <Stack.Navigator>
-        <Stack.Screen name="Onboarding" component={Onboarding} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </View>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Onboarding" component={Onboarding} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
   );
 };
 
